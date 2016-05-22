c0736358b27929c3f37d4adc0d5b099bf0d864d0
Ajinkya Avinash Aher <ajinkyaaher1@gmail.com>
2016-05-22 13:03:42 +0530

    Created New Project

---
+++ CRM App/CRM App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM_App.Startup))]
+namespace CRM_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
